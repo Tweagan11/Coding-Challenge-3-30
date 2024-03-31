bee6a55ce3471cfdf77f5b034431a0a22d57954a
Tweagan11 <123520140+Tweagan11@users.noreply.github.com>
2024-03-30 18:10:33 -0600

    Implemented a GUI so that the code is abstracted for the user.

--- NumberToRoman.py
+++ NumberToRoman.py
@@ -1,63 +1,6 @@
+ROMAN_NUMERALS = {'M': 1000.0, 'CM': 900.0, 'D': 500, 'CD': 400.0, 'C': 100, 'XC': 90.0, 'L': 50.0, 'XL': 40.0,
+                  'X': 10.0, 'IX': 9.0, 'V': 5.0, 'IV': 4.0, 'I': 1.0}
 
-
-ROMAN_NUMERALS = {'I': 1.0, 'IV': 4.0, 'V': 5.0, 'IX': 9.0, 'X': 10.0, 'XL': 40.0, 'L': 50.0, 'XC': 90.0, 'C': 100.0, 'CD': 400, 'D': 500.0, 'CM': 900, 'M': 1000.0}
-ROMAN_NUMERALS_2 = {'M': 1000.0, 'CM': 900.0, 'D': 500, 'CD': 400.0, 'C': 100, 'XC': 90.0, 'L': 50.0, 'XL': 40.0, 'X': 10.0, 'IX': 9.0, 'V': 5.0, 'IV': 4.0, 'I': 1.0}
-
-
-
-
-# def numeral_list(number):
-#     """Returns a list of roman numerals from a given number
-#     >>> numeral_list(22)
-#     ['X','X','I','I']"""
-#     roman_numerals = []
-#     while number > 0:
-#         if number / 1000.0 >= 1.0:
-#             roman_numerals.append('M')
-#             number -= 1000
-#         elif number / 900.0 >= 1:
-#             roman_numerals.append('C')
-#             roman_numerals.append('M')
-#             number -= 900
-#         elif number / 500.0 >= 1.0:
-#             roman_numerals.append('D')
-#             number -= 500.0
-#         elif number / 400.0 >= 1:
-#             roman_numerals.append('C')
-#             roman_numerals.append('D')
-#             number -= 400.0
-#         elif number / 100.0 >= 1.0:
-#             roman_numerals.append('C')
-#             number -= 100.0
-#         elif number / 90.0 >= 1:
-#             roman_numerals.append('X')
-#             roman_numerals.append('C')
-#             number -= 90.0
-#         elif number / 50.0 >= 1.0:
-#             roman_numerals.append('L')
-#             number -= 50.0
-#         elif number / 40.0 >= 1:
-#             roman_numerals.append('X')
-#             roman_numerals.append('L')
-#             number -= 40.0
-#         elif number / 10.0 >= 1.0:
-#             roman_numerals.append('X')
-#             number -= 10.0
-#         elif number / 9.0 >= 1:
-#             roman_numerals.append('I')
-#             roman_numerals.append('X')
-#             number -= 9.0
-#         elif number / 5.0 >= 1.0:
-#             roman_numerals.append('V')
-#             number -= 5.0
-#         elif number / 4.0 >= 1:
-#             roman_numerals.append('I')
-#             roman_numerals.append('V')
-#             number -= 4.0
-#         else:
-#             roman_numerals.append('I')
-#             number -= 1.0
-#     return roman_numerals
 
 def numeral_list_2(number):
     """Returns a list of roman numerals from a given number
@@ -65,12 +8,13 @@ def numeral_list_2(number):
     ['X','X','I','I']"""
     roman_numerals = []
     while number > 0:
-        for elem in ROMAN_NUMERALS_2:
-            if number / ROMAN_NUMERALS_2[elem] >= 1:
+        for elem in ROMAN_NUMERALS:
+            if number / ROMAN_NUMERALS[elem] >= 1:
                 roman_numerals.append(elem)
-                number -= ROMAN_NUMERALS_2[elem]
+                number -= ROMAN_NUMERALS[elem]
                 break
     return roman_numerals
+
 
 def number_to_roman(number):
     """Converts a number into a roman numeral
@@ -81,29 +25,13 @@ def number_to_roman(number):
     """
     try:
         number = float(number)
-    except ValueError as error:
-        print(error)
-        return
-
+    except ValueError:
+        return -1
+    if number >= 4000:
+        return -2
+    if number < 1:
+        return -3
     result_list = numeral_list_2(number)
     result_numeral = ''.join(result_list)
 
     return result_numeral
-
-
-
-
-# def number_parser(number, number_list=[])-> list[float]:
-#     """Returns a number broken up corresponding to its digit place.
-#     >>> number_parser(111)
-#     [1.0,10.0,100.0]
-#     >>> number_parser(9475)
-#     [5.0,70.0,400.0,9000.0]"""
-#     if number < 1:
-#         return number_list
-#     number_list += [float(number % 10)]
-#     return number_parser(number//10, number_list)
-
-
-if __name__ == '__main__':
-    print(number_to_roman(999))
---
+++ main.py
@@ -0,0 +1,77 @@
+import tkinter
+import customtkinter
+from RomanToNumber import roman_to_number
+from NumberToRoman import number_to_roman
+
+
+def helper_roman_to_number():
+    var = roman_entry.get()
+    result = roman_to_number(var)
+    if result == -1:
+        roman_final.configure(text="Invalid Roman Numeral", text_color="red")
+        return
+    roman_final.configure(text=f"Result: {result}", text_color="gray")
+    return
+
+
+def helper_number_to_roman():
+    var = number_entry.get()
+    result = number_to_roman(var)
+    # Invalid input detection
+    if result == -1:
+        number_final.configure(text="Invalid Number", text_color="red")
+        return
+    # Too big of a number
+    elif result == -2:
+        number_final.configure(text="Number too large, limit is 3999.", text_color="gray")
+        return
+    # Negative Numbers
+    elif result == -3:
+        result = number_to_roman(abs(float(var)))
+        number_final.configure(text=f"Invalid Number (Negative). Result for positive number: {result}", text_color="gray")
+        return
+    number_final.configure(text=f"Result: {result}", text_color="gray")
+    return
+
+
+# Add interface for user to access calculator
+customtkinter.set_appearance_mode('system')
+customtkinter.set_default_color_theme('blue')
+
+app = customtkinter.CTk()
+
+app.geometry('600x400')
+app.title('Roman Numeral Converter')
+
+roman_title = customtkinter.CTkLabel(app, text="Roman Numeral to Number")
+roman_title.pack(padx=10, pady=10)
+
+# Add Roman -> Number convertor
+numeral = tkinter.StringVar()
+roman_entry = customtkinter.CTkEntry(app, width=200, height=20, placeholder_text="Enter a Roman Numeral",
+                                     textvariable=numeral)
+roman_entry.pack(pady=10)
+
+roman_convertor_button = customtkinter.CTkButton(app, text="Convert", width=20, command=helper_roman_to_number)
+roman_convertor_button.pack(pady=10)
+
+roman_final = customtkinter.CTkLabel(app, text="")
+roman_final.pack(padx=10, pady=10)
+
+# Add Number -> Roman convertor
+number_title = customtkinter.CTkLabel(app, text="Number to Roman Numeral")
+number_title.pack(pady=10, padx=10)
+
+number = tkinter.DoubleVar()
+number_entry = customtkinter.CTkEntry(app, width=200, height=20, placeholder_text="Enter a Roman Numeral",
+                                      textvariable=number)
+number_entry.pack(pady=10)
+
+number_convertor_button = customtkinter.CTkButton(app, text="Convert", width=20, command=helper_number_to_roman)
+number_convertor_button.pack(pady=10)
+
+number_final = customtkinter.CTkLabel(app, text="")
+number_final.pack(padx=10, pady=10)
+
+if __name__ == "__main__":
+    app.mainloop()
